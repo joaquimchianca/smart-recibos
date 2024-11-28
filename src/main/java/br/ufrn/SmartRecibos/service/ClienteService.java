3f8bb7f68dcfae1fbf3df407ff0a584166be6c5e
Ipojuca Moraes da Cruz <ipojuca@expertsinfo.com.br>
2024-11-28 14:31:38 -0300

    Removido Interfaces desnecessarios

--- src/main/java/br/ufrn/SmartRecibos/service/ClienteService.java
+++ src/main/java/br/ufrn/SmartRecibos/service/ClienteService.java
@@ -1,8 +1,5 @@
 package br.ufrn.SmartRecibos.service;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,44 +7,13 @@ import br.ufrn.SmartRecibos.model.Cliente;
 import br.ufrn.SmartRecibos.repository.ClienteRepository;
 
 @Service
-public class ClienteService implements ICrudService<Cliente, Long> {
+public class ClienteService {
     @Autowired
     private ClienteRepository clienteRepository;
 
-    @Override
-    public List<Cliente> getAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
-    }
-
-    @Override
-    public Optional<Cliente> find(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'find'");
-    }
-
-    @Override
-    public List<Cliente> find(List<Long> ids) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'find'");
-    }
-
-    @Override
     public Cliente save(Cliente entityT) {
 
         return clienteRepository.save(entityT);
-    }
-
-    @Override
-    public Cliente update(Cliente entityT, Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
-    }
-
-    @Override
-    public void delete(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
     }
 
 }
