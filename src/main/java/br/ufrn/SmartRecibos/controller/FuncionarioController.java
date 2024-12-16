dcbb322e064886dd5c61a0ad8ed31cfc421e3022
Joaquim Chianca <joaquimchianca@gmail.com>
2024-12-16 17:23:28 -0300

    feat: Funcionario Controller

--- src/main/java/br/ufrn/SmartRecibos/controller/FuncionarioController.java
+++ src/main/java/br/ufrn/SmartRecibos/controller/FuncionarioController.java
@@ -1,25 +1,39 @@
 package br.ufrn.SmartRecibos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.ufrn.SmartRecibos.model.Funcionario;
 import br.ufrn.SmartRecibos.service.FuncionarioService;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("v1/funcionario")
 public class FuncionarioController {
-    @Autowired
-    private FuncionarioService funcionarioService;
+    private final FuncionarioService funcionarioService;
+
+    public FuncionarioController(FuncionarioService funcionarioService) {
+        this.funcionarioService = funcionarioService;
+    }
 
     @PostMapping
     public Funcionario save(@RequestBody Funcionario funcionario) {
-
         return funcionarioService.save(funcionario);
-
     }
 
+    @GetMapping
+    public List<Funcionario> findAll() {
+        return funcionarioService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Funcionario findById(@PathVariable Long id) {
+        return funcionarioService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        funcionarioService.delete(id);
+    }
 }
--- src/main/java/br/ufrn/SmartRecibos/service/FuncionarioService.java
+++ src/main/java/br/ufrn/SmartRecibos/service/FuncionarioService.java
@@ -6,13 +6,30 @@ import org.springframework.stereotype.Service;
 import br.ufrn.SmartRecibos.model.Funcionario;
 import br.ufrn.SmartRecibos.repository.FuncionarioRepository;
 
+import java.util.List;
+
 @Service
 public class FuncionarioService {
-    @Autowired
-    private FuncionarioRepository funcionarioRepository;
+    private final FuncionarioRepository funcionarioRepository;
+
+    public FuncionarioService(FuncionarioRepository funcionarioRepository) {
+        this.funcionarioRepository = funcionarioRepository;
+    }
+
+    public List<Funcionario> findAll() {
+        return funcionarioRepository.findAll();
+    }
+
+    public Funcionario findById(Long id) {
+        return funcionarioRepository.findById(id).orElse(null);
+    }
 
     public Funcionario save(Funcionario funcionario) {
         return funcionarioRepository.save(funcionario);
+    }
+
+    public void delete(Long id) {
+        funcionarioRepository.deleteById(id);
     }
 
 }
